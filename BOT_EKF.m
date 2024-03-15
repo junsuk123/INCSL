6a7b1dc15bc9bc1b88d85842ae9a019ac79bf9de
junsuk123 <junsuk55944587@gmail.com>
2024-03-15 15:22:14 +0900

    운동 모델 통합

--- BOT_EKF.m
+++ BOT_EKF.m
@@ -2,11 +2,11 @@ clc; clear; close all;
 
 %% 초기값 설정
 t = 1; % 측정 간격
-n = 100; % 측정 횟수
+n = 1000; % 측정 횟수
 
 %% Target initalize
 target_heading =deg2rad(30); % deg
-vel_target=20;
+vel_target=10;
 initial_target_State = [400 0 vel_target*cos(target_heading) vel_target*sin(target_heading)]; % x y x' y'
 %% Sensor initaialize
 sensor1_heading =deg2rad(80); % deg
@@ -19,9 +19,12 @@ initial_sensor2_State = [1000 0 vel_sensor2*cos(sensor2_heading) vel_sensor2*sin
 
 %% EKF Setting
 P = eye(4); % 4x4 단위 행렬
-Q = eye(4); % 4x4 단위 행렬
-R = eye(2); % 2x2 단위 행렬
-
+Q = 0.1*eye(4); % 4x4 단위 행렬
+R = 0.1*eye(2); % 2x2 단위 행렬
+f = [1 0 1 0;%model
+    0 1 0 1;
+    0 0 1 0;
+    0 0 0 1];
 %% Data initializing
 true_trajectory = zeros(n, 2);
 estimated_trajectory = zeros(n, 4);
@@ -29,59 +32,55 @@ sensor1_trajectory = zeros(n, 2);
 sensor2_trajectory = zeros(n, 2);
 
 target_state = initial_target_State';
+target_state_real=target_state;
 sensor1_state = initial_sensor1_State';
 sensor2_state = initial_sensor2_State';
-error=zeros(n,5);
+error=zeros(n,4);
 %% Main
 for i = 1:n
     % Prediction
     time=i*t;
-    target_state_real = motion_model(initial_target_State, time);
-    sensor1_state=motion_model(initial_sensor1_State,time);
-    sensor2_state=motion_model(initial_sensor2_State,time);
+    target_state_real = motion_model(target_state_real,f);
+    sensor1_state=motion_model(sensor1_state,f);
+    sensor2_state=motion_model(sensor2_state,f);
     
     true_trajectory(i, :) = target_state_real(1:2)';
     sensor1_trajectory(i, :) = sensor1_state(1:2)';
     sensor2_trajectory(i, :) = sensor2_state(1:2)';
-     f = [1 0 1 0;
-             0 1 0 1;
-             0 0 1 0;
-             0 0 0 1];
-     target_state=f*target_state;%convert xy to bearing
+    target_state=f*target_state;
 
     % linearlized model
-    H = measurement_model(target_state_real, sensor1_state, sensor2_state);
+    H = measurement_model(target_state_real', sensor1_state', sensor2_state');
     P = prediction_step(P, Q);
     
     % K gain
     K = P * H' * inv(H * P * H' + R);
     
     % measurement update
-    z = [bearing_measurement(target_state_real, sensor1_state); bearing_measurement(target_state_real, sensor2_state)];
-    z = z + [randn(1,1)*deg2rad(1); randn(1,1)*deg2rad(1)];
+    z = [bearing_measurement(target_state_real', sensor1_state'); bearing_measurement(target_state_real', sensor2_state')];
+    z = z + randn(2,1)*deg2rad(1);
     %State estimation
-    h= [bearing_measurement(target_state, sensor1_state); bearing_measurement(target_state, sensor2_state)];
+    h= [bearing_measurement(target_state, sensor1_state'); bearing_measurement(target_state, sensor2_state')];
     target_state = target_state + K * (z - h);
     P = (eye(4) - K * H) * P;
     
     % save result
     estimated_trajectory(i, :) = target_state(1:4)';
-    e=[target_state'-target_state_real]';
-    disp(det(P))
-    error(i,:)=[e(1);e(2); e(3);e(4);det(P)];
+    e=[target_state'-target_state_real']';
+    error(i,:)=[e(1);e(2); e(3);e(4)];
 end
 
 
 %% Plot
 figure;
-plot(true_trajectory(:, 1), true_trajectory(:, 2), 'k', 'LineWidth', 2); hold on;
+plot(true_trajectory(:, 1), true_trajectory(:, 2), 'k.', 'LineWidth', 2); hold on;
 plot(estimated_trajectory(:, 1), estimated_trajectory(:, 2), 'g.', 'LineWidth', 2);
 plot(sensor1_trajectory(:, 1), sensor1_trajectory(:, 2), 'r-', 'LineWidth', 2);
 plot(sensor2_trajectory(:, 1), sensor2_trajectory(:, 2), 'r-', 'LineWidth', 2);
 
 xlabel('X 위치');
 ylabel('Y 위치');
-title('타겟의 궤적과 추정 궤적'); 
+title('타겟의 궤적과 추정 궤적(m)'); 
 legend('실제 궤적', '추정 궤적', '센서 위치');
 
 figure;
@@ -89,7 +88,7 @@ plot(1:n, error(:, 1), 'm', 'LineWidth', 2);hold on;
 plot(1:n, error(:, 2), 'y', 'LineWidth', 2);
 xlabel('시간');
 ylabel('거리 오차');
-title('타겟 위치 추정의 거리 오차');
+title('타겟 위치 추정의 거리 오차(m)');
 legend('Position Error_x', 'Position Error_y');
 
 figure;
@@ -97,19 +96,13 @@ plot(1:n, error(:, 3), 'm', 'LineWidth', 2);hold on;
 plot(1:n, error(:, 4), 'y', 'LineWidth', 2);
 xlabel('시간');
 ylabel('속도 오차');
-title('타겟 위치 추정의 거리 오차');
+title('타겟 위치 추정의 속도 오차(m/s)');
 legend('Velocity Error_x', 'Velocity Error_y');
-
-figure;
-plot(1:n, rad2deg(error(:, 5)), 'g', 'LineWidth', 2);hold on;
-xlabel('시간');
-ylabel('detP');
-title('P');
 
 %% Function
 % physical model
-function x = motion_model(x, t)
-    x = [x(1)+x(3)*t x(2)+x(4)*t x(3) x(4)];
+function x = motion_model(x,f)
+    x=f*x;
 end
 
 % measurement_model
@@ -133,9 +126,9 @@ end
 
 % P_prediction
 function P = prediction_step(P,Q)
- F = [1 0 1 0;
-             0 1 0 1;
-             0 0 1 0;
-             0 0 0 1];
-    P = F*P*F' + Q;
+F = [1 0 1 0;
+    0 1 0 1;
+    0 0 1 0;
+    0 0 0 1];
+P = F*P*F' + Q;
 end
